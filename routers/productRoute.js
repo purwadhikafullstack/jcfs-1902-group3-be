6f21a1306ded6fdd86868ffadaf7d4d2910111d6
alifjunjunan <junjunanalif@gmail.com>
2022-04-10 16:28:40 +0700

    develop-01: menerapkan Readtoken

--- routers/productRoute.js
+++ routers/productRoute.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const {productController} = require('../controllers')
+const { readToken } = require('../supports/enkripsi')
 
 router.get('/', productController.getProduct)
-router.post('/', productController.addproduct)
-router.patch('/:idproduct', productController.updateProduct)
-router.patch('/image/:idimage', productController.updateImageProduct)
-router.delete('/:idproduct', productController.softDelete)
+router.post('/', readToken, productController.addproduct)
+router.patch('/:idproduct', readToken, productController.updateProduct)
+router.patch('/image/:idimage', readToken, productController.updateImageProduct)
+router.delete('/:idproduct', readToken, productController.softDelete)
 module.exports = router
--- routers/transactionRoute.js
+++ routers/transactionRoute.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
-const {transactionController} = require('../controllers')
+const { transactionController } = require('../controllers')
+const { readToken } = require('../supports/enkripsi')
 
-router.post('/carts', transactionController.addCart)
-router.patch('/carts/:idcart', transactionController.updateQtyCart)
-router.get('/carts', transactionController.getCart)
-router.delete('/carts/:idcart', transactionController.deleteCart)
+router.post('/carts', readToken, transactionController.addCart)
+router.patch('/carts/:idcart', readToken, transactionController.updateQtyCart)
+router.get('/carts', readToken, transactionController.getCart)
+router.delete('/carts/:idcart', readToken, transactionController.deleteCart)
 
 module.exports = router
