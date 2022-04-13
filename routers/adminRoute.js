85d0b0c8eb14fc79c8a428b34108b29126f9b35f
boblightning <satrianugraha4000@gmail.com>
2022-04-14 04:16:44 +0700

    WOOD-07-SATRIA: WOOD-44

--- routers/adminRoute.js
+++ routers/adminRoute.js
@@ -3,8 +3,13 @@ const {adminController} = require('../controllers')
 const { readToken } = require("../supports/enkripsi");
 
 router.get(`/getwarehouse`, adminController.getWarehouse);
-router.get(`/getadmin`, adminController.getAdmin);
 router.post(`/addwarehouse`, readToken, adminController.addWarehouse);
+router.patch(`/updatewarehouse/:idwarehouse`,readToken, adminController.updateWarehouse);
+router.delete(`/deletewarehouse/:idwarehouse`,readToken, adminController.deleteWarehouse);
+
+router.get(`/getadmin`, adminController.getAdmin);
 router.post(`/addadmin`, adminController.addAdmin);
+router.patch(`/updateadmin/:iduser`,readToken, adminController.updateAdmin);
+router.delete(`/deleteadmin/:iduser`,readToken, adminController.deleteAdmin);
 
 module.exports = router
