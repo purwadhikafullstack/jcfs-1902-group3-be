5e23cfba98b4ad93f1a3b5b1288dcb1d85459b67
alifjunjunan <junjunanalif@gmail.com>
2022-04-14 21:05:48 +0700

    WOOD-17-01: fungsi konfirmasi barang diterima

--- routers/transactionRoute.js
+++ routers/transactionRoute.js
@@ -8,6 +8,7 @@ router.get('/carts', readToken, transactionController.getCart)
 router.delete('/carts/:idcart', readToken, transactionController.deleteCart)
 router.post('/ongkos', readToken, transactionController.getOngkir)
 router.post('/checkout', readToken, transactionController.checkout)
+router.patch('/:idtransaksi', readToken, transactionController.UserTerimaBarang)
 router.get('/', readToken, transactionController.getTransaksi)
 router.patch('/:idtransaksi', readToken, transactionController.unggahReceipt)
 
