f8014fefbe980eff1790b3dd6066401919bd2c27
alifjunjunan <junjunanalif@gmail.com>
2022-04-20 17:18:11 +0700

    WOOD-32-01: konfirmasi pesanan oleh admin

--- routers/transactionRoute.js
+++ routers/transactionRoute.js
@@ -8,7 +8,7 @@ router.get('/carts', readToken, transactionController.getCart)
 router.delete('/carts/:idcart', readToken, transactionController.deleteCart)
 router.post('/ongkos', readToken, transactionController.getOngkir)
 router.post('/checkout', readToken, transactionController.checkout)
-router.patch('/:idtransaksi', readToken, transactionController.UserTerimaBarang)
+router.patch('/:idtransaksi', readToken, transactionController.konfirmasiPesanan)
 router.get('/', readToken, transactionController.getTransaksi)
 router.patch('/upload/:idtransaksi', readToken, transactionController.unggahReceipt)
 
